2a72a936aa0b6fd2e4d88999958a7130b1874ef7
Bad Wolf <sidelnickov.roma2013@yandex.ru>
2018-10-27 22:29:19 +0300

    v0.0.3
    Первая минимальная версия генератора

--- Assets/NARMALDY/Scripts/MapGen/CreaterMap.cs
+++ Assets/NARMALDY/Scripts/MapGen/CreaterMap.cs
@@ -8,13 +8,49 @@ public class CreaterMap : MonoBehaviour{
 
     [SerializeField] Tilemap FloorMap;
     [SerializeField] Tile FloorTile;
+    [SerializeField] Tilemap ColMap;
+    [SerializeField] Tile Wall;
+    [SerializeField] Tile Roof;
+    [SerializeField] GameObject Player;
+
     [SerializeField] uint LevelLength, LevelHeight, MaxWidthRoomInLevel, MaxHightRoomInLevel,seed;
 
     public void Gen()
     {
-        Debug.Log(FloorMap.size);
+        seed++;
         MapGenerator1 generator1 = new MapGenerator1();
         generator1.SetGeneratorSettings(LevelLength, LevelHeight, MaxWidthRoomInLevel, MaxHightRoomInLevel,(int)seed);
         generator1.Gen(FloorMap, FloorTile);
+
+        Debug.Log(FloorMap.size);
+        for (int i = 0; i < FloorMap.size.x; i++)
+        {
+            for (int j = 0; j < FloorMap.size.y; j++)
+            {
+                if(FloorMap.GetTile(new Vector3Int(i,j,0)) == FloorTile)
+                {
+                    if (FloorMap.GetTile(new Vector3Int(i, j + 1, 0)) == null)
+                    {
+                        ColMap.SetTile(new Vector3Int(i, j + 1, 0), Wall);
+                        if (FloorMap.GetTile(new Vector3Int(i, j + 2, 0)) == null)
+                            ColMap.SetTile(new Vector3Int(i, j + 2, 0), Roof);
+                    }
+                    if (FloorMap.GetTile(new Vector3Int(i, j - 1, 0)) == null && ColMap.GetTile(new Vector3Int(i, j - 1, 0)) == null)
+                    {
+                        ColMap.SetTile(new Vector3Int(i, j - 1, 0), Roof);
+                    }
+                    if (FloorMap.GetTile(new Vector3Int(i- 1, j , 0)) == null && ColMap.GetTile(new Vector3Int(i- 1, j , 0)) == null)
+                    {
+                        ColMap.SetTile(new Vector3Int(i- 1, j , 0), Roof);
+                    }
+                    if (FloorMap.GetTile(new Vector3Int(i+1, j , 0)) == null && ColMap.GetTile(new Vector3Int(i+1, j , 0)) == null)
+                    {
+                        ColMap.SetTile(new Vector3Int(i+1, j , 0), Roof);
+                    }
+                }
+            }
+        }
+
+        Player.transform.position = generator1.AllRooms[0].GetCenterPosition();
     }
 }
--- Assets/NARMALDY/Scripts/MapGen/MapGenerator1.cs
+++ Assets/NARMALDY/Scripts/MapGen/MapGenerator1.cs
@@ -57,16 +57,18 @@ namespace Map
             return new Vector2(Position.x + _Width / 2, Position.y + _Hight / 2);
         }
     }
-    
+
     public class MapGenerator1
     {
         #region Настройки генератора
-        private uint MaxWidthRoom = 10;
+        private uint MaxWidthRoom = 10; // Максимальная ширина комнаты 
         private uint MaxHightRoom = 10;
-        private uint MinWidthRoom = 3;
+        private uint MinWidthRoom = 3; // минимальная высота комнаты
         private uint MinHightRoom = 3;
-        private uint MaxRoomX = 10;
-        private uint MaxRoomY = 10;
+        private uint MaxRoomX = 10; // максимум комнат на оси X
+        private uint MaxRoomY = 10; // максимум комнат на оси Y
+        private int CountOfCorridors = 1; // воличество выходящих коридоров
+        private int DegreeOfSmoothing = 1; // количество поворотов между комнатами
         #endregion
 
         private List<List<Room>> RoomMap;
@@ -85,7 +87,7 @@ namespace Map
             MaxRoomY = LevelHeight;
             MaxWidthRoom = MaxWidthRoomInLevel;
             MaxHightRoom = MaxHightRoomInLevel;
-            if(Seed != 0) Random.InitState(Seed);
+            if (Seed != 0) Random.InitState(Seed);
         }
 
         /// <summary>
@@ -97,14 +99,14 @@ namespace Map
             List<List<Room>> OnAxisX = new List<List<Room>>();
             OnAxisX.Add(new List<Room>());
             OnAxisX[0].Add(new Room(
-                new Vector2Int(0, (int)Random.Range(0,(MaxHightRoom * MaxRoomY)-MinHightRoom)), 
-                (uint)Random.Range(MinWidthRoom, MaxWidthRoom), 
-                (uint)Random.Range(MinHightRoom, MaxHightRoom)));
+                new Vector2Int(0, (int)Random.Range(0, (MaxHightRoom * MaxRoomY) - MinHightRoom) + Random.Range(0, (int)(MaxHightRoom * MaxRoomY))),
+                (uint)Random.Range(MinWidthRoom, MaxWidthRoom),
+                (uint)Random.Range(MinHightRoom, MaxHightRoom))); // создание начальной комнаты
 
-            for (int i = 1; i < MaxRoomX; i++)
+            for (int i = 1; i < MaxRoomX; i++) // цикл по всем осям
             {
                 OnAxisX.Add(new List<Room>());
-                int CountRoonInAxis = (int)Random.Range(1, MaxRoomY);
+                //int CountRoonInAxis = (int)Random.Range(1, MaxRoomY);
                 int CellLeft = (int)(MaxHightRoom * MaxRoomY);
 
                 int
@@ -124,24 +126,110 @@ namespace Map
                     LastHightRoom = HightRoom;
                     HightRoom = Random.Range((int)MinHightRoom, (int)MaxHightRoom);
                     OffsetY = Random.Range(0, (int)MaxHightRoom - HightRoom) + Random.Range(0, (int)(MaxHightRoom * MaxRoomY));
-                    PosOnY = PosOnY  + LastHightRoom + OffsetY + Random.Range((int)MinHightRoom, (int)(MaxHightRoom/MaxRoomY));
+                    PosOnY = PosOnY + LastHightRoom + OffsetY + Random.Range((int)MinHightRoom, (int)(MaxHightRoom / MaxRoomY));
                     OnAxisX[i].Add(new Room(new Vector2Int(PosOnX, PosOnY), (uint)WidthRoom, (uint)HightRoom));
-
+                    //CountRoonInAxis--; ountRoonInAxis>0
                 } while (PosOnY < CellLeft);
             }
             return OnAxisX;
         }
 
-        void CreateCorridors()
+        void Swap(ref int a, ref int b)
         {
-
+            int k = b;
+            b = a;
+            a = k;
         }
 
+        void DrawCorridors(Room StartRoom, Room EndRoom)
+        {
+            Vector2Int StartPos = new Vector2Int(
+                Random.Range(StartRoom.Position.x, StartRoom.Position.x + (int)StartRoom.Width),
+                Random.Range(StartRoom.Position.y, StartRoom.Position.y + (int)StartRoom.Hight));
+
+            Vector2Int EndPos = new Vector2Int(
+                Random.Range(EndRoom.Position.x, EndRoom.Position.x + (int)EndRoom.Width),
+                Random.Range(EndRoom.Position.y, EndRoom.Position.y + (int)EndRoom.Hight));
+
+            //int DistansForX = (EndPos.x - StartPos.x) ;
+            //int DistansForY = (EndPos.y - StartPos.y) ;
+
+
+            //int x = StartPos.x,
+            //    y = StartPos.y;
+
+
+            int
+                x1 = StartPos.x,
+                y1 = StartPos.y,
+                x2 = EndPos.x,
+                y2 = EndPos.y,
+                x3;
+
+            if (x1 < x2)
+            {
+                x3 = Random.Range(x1, x2);
+                for (int x = x1; x<= x3; x++)
+                    _Floor.SetTile(new Vector3Int(x, y1,0),_FloorTile);
+                for (int x = x3; x < x2; x++)
+                    _Floor.SetTile(new Vector3Int(x, y2, 0), _FloorTile);
+            }
+            else
+            {
+                x3 = Random.Range(x2, x1);
+                for (int x = x2; x<= x3; x++)
+                    _Floor.SetTile(new Vector3Int(x, y2, 0), _FloorTile);
+                for (int x = x3; x < x1; x++)
+                    _Floor.SetTile(new Vector3Int(x, y1, 0), _FloorTile);
+            }
+            if (y1 > y2)
+                Swap(ref y1,ref y2);
+            for (int y = y1; y < y2; y++)
+                _Floor.SetTile(new Vector3Int(x3, y,0), _FloorTile);
+
+            
+        }
+
+        void CreateCorridors(List<Room> AllRooms)
+        {
+            List<List<float>> AllDistans = new List<List<float>>();
+            for (int i = 0; i < AllRooms.Count; i++)
+            {
+                AllDistans.Add(new List<float>());
+                for (int j = 0; j < AllRooms.Count; j++)
+                {
+                    AllDistans[i].Add(Vector2.Distance(AllRooms[i].GetCenterPosition(), AllRooms[j].GetCenterPosition()));
+                }
+            }
+
+            for (int i = 0; i < AllDistans.Count - 1; i++)
+            {
+                int FirstRoom = i, SecondRoom = i + 1;
+                float min = AllDistans[FirstRoom][SecondRoom];
+                for (int j = i + 1; j < AllDistans.Count; j++)
+                    if (AllDistans[i][j] < min)
+                    {
+                        min = AllDistans[i][j];
+                        FirstRoom = i;
+                        SecondRoom = j;
+                    }
+                DrawCorridors(AllRooms[FirstRoom], AllRooms[SecondRoom]);
+            }
+
+        }
+        Tilemap _Floor;
+        Tile _FloorTile;
+        public List<Room> AllRooms;
         public void Gen(Tilemap tileMap, Tile tile)
         {
+            _Floor = tileMap;
+            _FloorTile = tile;
+
             tileMap.ClearAllTiles();
             RoomMap = CreateFloor();
-            List<Room> AllRooms = new List<Room>();
+            AllRooms = new List<Room>();
+
+
             for (int iteratorOnAxisX = 0; iteratorOnAxisX < RoomMap.Count; iteratorOnAxisX++)
             {
                 for (int iteratorOnAxisY = 0; iteratorOnAxisY < RoomMap[iteratorOnAxisX].Count; iteratorOnAxisY++)
@@ -157,6 +245,7 @@ namespace Map
                     }
                 }
             }
+            CreateCorridors(AllRooms);
 
             
         }
